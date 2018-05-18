a04d4737882fb357974a783b4a83d4b557aa396d
luke <azathothbeats@gmail.com>
2018-05-18 13:41:57 -0400

    React app connected to express server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 
-app.get('/', (req, res) => {
+app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hey from express'})
 })
 
