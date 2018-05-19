d1969998143edb998c6b8fd0c36401c835dba414
luke <azathothbeats@gmail.com>
2018-05-18 23:48:33 -0400

    Dummy data now parsed and displayed in React app

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,6 @@ class App extends Component {
     this.callApi()
       .then(res => this.setState({response: res.express}))
       .catch(err => console.log(err))
-    console.log(this.state.response)
   }
 
   callApi = async () => {
@@ -22,10 +21,18 @@ class App extends Component {
     return body
   }
 
+  showAppointment = () => {
+    console.log(this.state.response.client)
+  }
+
   render() {
     return (
       <div className="App">
         <h1>{JSON.stringify(this.state.response, null, 2)}</h1>
+        <h2>Client: {this.state.response.client}</h2>
+        <h3>Date booked: {this.state.response.date}</h3>
+        <h3>Duration booked: {this.state.response.length}</h3>
+        <h3>Scheduled on: {this.state.response.booked_on}</h3>
       </div>
     );
   }
