78a110e17dabf6a8694cff5e19cb6920ac6d77f3
luke <azathothbeats@gmail.com>
2018-05-19 16:34:07 -0400

    dates in dummy data now properly parsed

--- model/dummydata.js
+++ model/dummydata.js
@@ -1,8 +1,18 @@
+let today = new Date()
+let tomorrow = new Date()
+let dd = today.getDate()
+let dd2 = today.getDate()+5
+let mm = today.getMonth()+1 //January is 0!
+let yyyy = today.getFullYear()
+
+today = mm + '/' + dd + '/' + yyyy
+tomorrow = mm + '/' + dd2 + '/' + yyyy
+
 const appointment = {
   client: "Luke Camelo",
-  date: "5/20/2018",
+  date: tomorrow,
   length: 45,
-  booked_on: "5/18/18"
+  booked_on: today
 }
 
 module.exports = appointment
