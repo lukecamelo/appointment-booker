a90621b0d9b43b491c0b951cce5eef6ba27917ff
luke <azathothbeats@gmail.com>
2018-05-28 16:32:16 -0400

    Clientside start-end times now parsed and pretty

--- client/src/components/Appointment.js
+++ client/src/components/Appointment.js
@@ -8,15 +8,17 @@ class Appointment extends Component {
 
     const { _id, client, date, endTime, startTime } = this.props.appointment
     const { index, deleteAppointment } = this.props
-
-
+    
+    const parsedStart = startTime.split(' ')[4]
+    const parsedEnd = endTime.split(' ')[4]
+    
     return (
       <tr className='Appointment'>
         <th>{index}</th>
         <td>{client}</td>
         <td>{date}</td>
-        <td>{startTime}</td>
-        <td>{endTime}</td>
+        <td>{parsedStart}</td>
+        <td>{parsedEnd}</td>
         <td><a className='button is-danger' onClick={() => deleteAppointment(_id)}>Delete</a></td>
         <td><Link className='button is-primary' to={`/appointments/${_id}`}>Edit</Link></td>
       </tr>
