b38c442563bd0a088f8aef99f3320857a0ee7451
Luke Camelo <azathothbeats@gmail.com>
2018-07-27 23:12:29 -0400

    Cleaned up server.js code

--- server.js
+++ server.js
@@ -14,52 +14,52 @@ const bcrypt = require('bcrypt-nodejs')
 const passport = require('passport')
 const Strategy = require('passport-local').Strategy
 
-passport.use('local-login', new Strategy(
-  function(username, password, cb) {
+passport.use(
+  'local-login',
+  new Strategy(function(username, password, cb) {
     User.findOne({ username: username }, (err, user) => {
-
-      if (err) { return cb(err) }
+      if (err) {
+        return cb(err)
+      }
 
       if (!user) {
-        
         console.log('invalid user/pass')
         return cb(null, false)
       }
 
-      if (!user.validPassword(password) && user.password != password) { return cb(null, false) }
+      if (!user.validPassword(password) && user.password != password) {
+        return cb(null, false)
+      }
 
       console.log('success!')
       return cb(null, user)
     })
-  }
-))
+  })
+)
 
-passport.use('local-signup', new Strategy(
-  function(username, password, done) {
-    User.findOne({ username: username}, (err, user) => {
-      if (err)
-        return done(err)
+passport.use(
+  'local-signup',
+  new Strategy(function(username, password, done) {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) return done(err)
 
       if (user) {
         return done(null, false)
       } else {
-
         const newUser = new User()
 
         newUser.username = username
         newUser.password = newUser.generateHash(password)
 
-        newUser.save((err) => {
-          if (err)
-            throw err
+        newUser.save(err => {
+          if (err) throw err
 
           return done(null, newUser)
         })
       }
-
     })
-  }
-))
+  })
+)
 
 passport.serializeUser((user, cb) => {
   cb(null, user.id)
@@ -67,7 +67,9 @@ passport.serializeUser((user, cb) => {
 
 passport.deserializeUser((id, cb) => {
   User.findById(id, (err, user) => {
-    if (err) { return cb(err) }
+    if (err) {
+      return cb(err)
+    }
     cb(null, user)
   })
 })
@@ -77,40 +79,52 @@ app.use(bodyParser.json())
 app.use(require('morgan')('combined'))
 app.use(require('cookie-parser')())
 app.use('/api', routes)
-app.use(require('express-session')({ secret: 'express secret', resave: false, saveUninitialized: false }))
+app.use(
+  require('express-session')({
+    secret: 'express secret',
+    resave: false,
+    saveUninitialized: false
+  })
+)
 app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-mongoose.connect('mongodb://lukecamelo:password@ds117509.mlab.com:17509/appointment_db', {
-  reconnectInterval: 1000
-})
+mongoose.connect(
+  'mongodb://lukecamelo:password@ds117509.mlab.com:17509/appointment_db',
+  {
+    reconnectInterval: 1000
+  }
+)
 
 db.on('error', console.error.bind(console, 'connection error:'))
 
 app.get('/login', (req, res) => {
   if (req.user) {
-    return res.json({user: req.user})
-  }
-  else {
-    return res.json({user: 'nobody logged in.'})
+    return res.json({ user: req.user })
+  } else {
+    return res.json({ user: 'nobody logged in.' })
   }
   res.redirect('/')
 })
 
-app.post('/login', 
+app.post(
+  '/login',
   passport.authenticate('local-login', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/')
     return res.json()
-})
+  }
+)
 
-app.post('/signup', 
-  passport.authenticate('local-signup', { failureRedirect: '/signup'}),
+app.post(
+  '/signup',
+  passport.authenticate('local-signup', { failureRedirect: '/signup' }),
   function(req, res) {
     res.redirect('/')
-})
+  }
+)
 
 app.get('/logout', (req, res) => {
   req.logOut()
@@ -119,7 +133,7 @@ app.get('/logout', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 app.listen(port, () => console.log(`listening on port ${port}`))
